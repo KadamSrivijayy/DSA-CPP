07c04085c5a3c226ec331dd2262985bfd6c2c6c5
KadamSrivijayy <srivijaykadam09@gmail.com>
2026-09-17 00:13:47 +0530

    comment

---
+++ BubbleSort.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+using namespace std;
+
+void bubbleSort(int arr[], int size){
+    for(int i = 0; i < size - 1; i++){
+        
+        for(int j = 0; j < size - i - 1; j++){
+            if(arr[j] > arr[j+1]){
+                swap(arr[j], arr[j+1]);
+            }
+        }
+        
+    }
+}
+
+
+void printArray(int arr[], int size){
+    for(int i = 0; i< size - 1; i++){
+        cout << arr[i] << " ";
+    }
+}
+
+
+
+int main(){
+    int size = 5;
+    int arr[] = {98, 33, 21, 44, 93};
+
+    bubbleSort(arr, size);
+    printArray(arr, size);
+
+    return 0;
+}
+
+
+
