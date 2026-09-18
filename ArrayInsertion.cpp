1ff28b98cf881a4567e7924e77830fde6bfc8817
KadamSrivijayy <srivijaykadam09@gmail.com>
2026-09-18 23:46:27 +0530

    Insertion

---
+++ ArrayInsertion.cpp
@@ -0,0 +1,79 @@
+#include<iostream>
+using namespace std;
+
+
+// int main(){
+//     int arr[10] = {10, 20, 30, 40, 50};
+//     int size = 5;
+
+//     int pos = 2;
+//     int value = 25;
+
+
+//     for(int i = size; i > pos; i--){
+//         arr[i] = arr[i-1];
+//     }
+
+
+//     arr[pos] = value;
+//     size++;
+
+//     for(int i=0; i<size; i++){
+//         cout<< arr[i]<< " ";
+//     }
+
+
+//     return 0;
+// }
+
+
+
+
+
+// int main(){
+//     int arr[10] = {10, 20, 30, 40, 50};
+//     int size = 5;
+
+//     int pos = 0;
+//     int value;
+//     cout<<"Enter the value to be inserted: ";
+//     cin>>value;
+
+
+//     for(int i = size; i > pos; i--){
+//         arr[i] = arr[i-1];
+//     }
+
+
+//     arr[pos]= value;
+//     size++;
+
+
+//     for(int i = 0; i< size; i++){
+//         cout<< arr[i]<< " ";
+//     }
+
+//     return 0;
+// }
+
+
+
+int main(){
+    int arr[10] = {10, 20, 30, 40, 50};
+    int size = 5;
+
+    int pos = size;
+    int value = 60;
+
+
+    arr[pos] = value;
+    size++;
+
+
+    for(int i = 0; i<size ; i++){
+        cout<< arr[i]<< " ";
+    }
+
+    return 0;
+
+}
