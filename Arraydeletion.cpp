663c5a386f1b2692595f602cf5fdb44f6dead235
KadamSrivijayy <srivijaykadam09@gmail.com>
2026-09-18 23:56:31 +0530

    Deletion

--- Arraydeletion.cpp
+++ Arraydeletion.cpp
@@ -1,17 +1,42 @@
 #include<iostream>
 using namespace std;
 
+// int main(){
+//     int arr[] ={10, 20, 30, 40, 50};
+//     int size = 5;
+
+//     int pos = 2;
+//     for(int i = pos; i < size -1; i++){
+//         arr[i] = arr[i+1];
+//     }
+
+
+//     size--;
+
+
+//     for(int i = 0; i < size; i++){
+//         cout<<arr[i]<<" ";
+//     }
+
+//     return 0;
+// }
+
+
+
+
+
+// Delete an element from the beginning
+
 int main(){
-    int arr[] ={10, 20, 30, 40, 50};
+    int arr[] = {10, 20, 30, 40, 50};
     int size = 5;
 
-    int pos = 2;
-    for(int i = pos; i < size -1; i++){
+
+    int pos = 0;
+    for(int i = pos; i < size - 1; i++){
         arr[i] = arr[i+1];
     }
-
-
-    size--;
+    size--; 
 
 
     for(int i = 0; i < size; i++){
@@ -19,4 +44,7 @@ int main(){
     }
 
     return 0;
+
 }
+
+ 
