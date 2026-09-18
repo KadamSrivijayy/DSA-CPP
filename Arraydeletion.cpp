d6f0485051c99ee5f9e210502b2b3dcd18c9077c
KadamSrivijayy <srivijaykadam09@gmail.com>
2026-09-18 23:32:28 +0530

    Deletion

---
+++ Arraydeletion.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int arr[] ={10, 20, 30, 40, 50};
+    int size = 5;
+
+    int pos = 2;
+    for(int i = pos; i < size -1; i++){
+        arr[i] = arr[i+1];
+    }
+
+
+    size--;
+
+
+    for(int i = 0; i < size; i++){
+        cout<<arr[i]<<" ";
+    }
+
+    return 0;
+}
